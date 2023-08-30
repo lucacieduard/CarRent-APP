ccafd033a62090db1309a443f0e71456b079b666
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-08-30 19:33:51 +0300

    basic routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.scss";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 function App() {
-  return <>Hello word</>;
+  return (
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="*" element={<h1>Not found!</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Cars/Cars.tsx
@@ -0,0 +1,7 @@
+import "./Cars.scss";
+
+const Cars = () => {
+  return <div>Cars</div>;
+};
+
+export default Cars;
