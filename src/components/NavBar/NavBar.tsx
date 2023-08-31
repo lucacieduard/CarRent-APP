22255b84b93e9ea1212d93f62fe59cb5ed85be5d
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-08-31 15:06:30 +0300

    ui fix navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,33 +5,35 @@ import {
   faHeart,
   faBell,
 } from "@fortawesome/free-solid-svg-icons";
+import FullPageLayout from "../../layout/FullPage/FullPageLayout";
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <span className="logo">MORENT</span>
-
-      <div className="icons">
-        <span className="icon">
-          <FontAwesomeIcon icon={faHeart} className="faicon" />
-        </span>
-        <span className="icon">
-          <FontAwesomeIcon icon={faBell} className="faicon" />
-        </span>
+    <FullPageLayout>
+      <div className="navbar">
+        <span className="logo">MORENT</span>
+        <div className="icons">
+          <span className="icon">
+            <FontAwesomeIcon icon={faHeart} className="faicon" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faBell} className="faicon" />
+          </span>
+        </div>
+        <img
+          src="/profile.png"
+          alt="profile picture"
+          className="profile_pic"
+        ></img>
+        <div className="search">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
+          <input
+            type="text"
+            className="input"
+            placeholder="Search something here..."
+          />
+        </div>
       </div>
-      <img
-        src="/profile.png"
-        alt="profile picture"
-        className="profile_pic"
-      ></img>
-      <div className="search">
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
-        <input
-          type="text"
-          className="input"
-          placeholder="Search something here..."
-        />
-      </div>
-    </div>
+    </FullPageLayout>
   );
 };
 
