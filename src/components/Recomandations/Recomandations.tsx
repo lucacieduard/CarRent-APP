e370093a894898d1db3ddd439d712bef08c1b0fa
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-09-07 21:35:36 +0300

    show more/less functionality added

--- src/components/Recomandations/Recomandations.tsx
+++ src/components/Recomandations/Recomandations.tsx
@@ -1,19 +1,31 @@
 import CarCard from "../CarCard.tsx/CarCard";
 import "./Recomandations.scss";
+import { useState } from "react";
 
 const Recomandations = () => {
+  const [carsNumber, setCarsNumber] = useState<number>(8);
+
+  const clickHandler = (add: boolean): void => {
+    setCarsNumber((prev) => {
+      if (add) return prev + 8;
+      else return prev - 8;
+    });
+  };
   return (
     <div className="recomandations container_w">
       <span className="title">Recomendation Car</span>
       <div className="recomandationCars">
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
-        <CarCard recomandation={true} />
+        {Array(carsNumber)
+          .fill(0)
+          .map((_dummy, index) => {
+            return <CarCard key={index} recomandation={true} />;
+          })}
+      </div>
+      <div className="buttons">
+        <button onClick={() => clickHandler(true)}>Show more car</button>
+        {carsNumber > 8 && (
+          <button onClick={() => clickHandler(false)}>Show less car</button>
+        )}
       </div>
     </div>
   );
