c4230e76001825757fdc2c325fd98fb52350f7a3
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-09-07 20:58:42 +0300

    use card in multiple components

--- src/components/CarCard.tsx/CarCard.tsx
+++ src/components/CarCard.tsx/CarCard.tsx
@@ -3,17 +3,27 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import "./CarCard.scss";
 import { faGasPump, faO, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
-const CarCard = () => {
+type Props = {
+  recomandation: boolean;
+};
+const CarCard = (props: Props) => {
   return (
-    <div className="cardCar">
+    <div className={`cardCar ${props.recomandation ? "recCardCar" : ""}`}>
       <div className="cardHeader">
         <span className="carName">Koenigsegg</span>
         <FontAwesomeIcon icon={faHeart} color="gray" className="heart" />
       </div>
       <span className="carType">Sport</span>
-      <div className="cardMain">
-        <img src="/image_7.png" className="mainImg" />
-        <ul className="specifications">
+      <div className={`cardMain ${props.recomandation ? "recCardMain" : ""}`}>
+        <img
+          src="/image_7.png"
+          className={`mainImg ${props.recomandation ? "recMainImg" : ""}`}
+        />
+        <ul
+          className={`specifications ${
+            props.recomandation ? "recSpecifications" : ""
+          }`}
+        >
           <li className="spec">
             <FontAwesomeIcon icon={faGasPump} className="specIcon" />
             <span className="specText">90 L</span>
