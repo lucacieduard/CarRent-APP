ac3cafebfab83c4b73dd9e073c9e2e0a1921eb4f
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-09-28 15:06:59 +0300

    admin layout ui

--- src/layout/AdminLayout/AdminLayout.tsx
+++ src/layout/AdminLayout/AdminLayout.tsx
@@ -1,15 +1,108 @@
 import React from "react";
 import "./AdminLayout.scss";
 import FullPageLayout from "../FullPage/FullPageLayout";
+import {
+  faArrowRightFromBracket,
+  faBars,
+  faCarSide,
+  faHouse,
+  faPlus,
+  faUsers,
+  faWallet,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const [mobileMenu, setMobileMenu] = useState(false);
   return (
-    <FullPageLayout>
-      <div className="container_w adminContainer">
-        <div className="sidevarAdmin">sidebar </div>
+    <div className="globalAdmin">
+      <FullPageLayout>
+        <div className="container_w adminContainer">
+          {!mobileMenu && (
+            <FontAwesomeIcon
+              icon={faBars}
+              onClick={() => setMobileMenu(true)}
+              className="bars hideWeb"
+            />
+          )}
+          <div className={`sideBarAdminMobile ${mobileMenu ? "" : "show"}`}>
+            <FontAwesomeIcon
+              icon={faXmark}
+              onClick={() => setMobileMenu(false)}
+              className="bars hideWeb"
+            />
+            <ul className="sidebarNav">
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <li>
+                  <FontAwesomeIcon icon={faHouse} />
+                  DashBoard
+                </li>
+              </NavLink>
 
-        <div>{children}</div>
-      </div>
-    </FullPageLayout>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <li>
+                  <FontAwesomeIcon icon={faCarSide} />
+                  Cars
+                </li>
+              </NavLink>
+
+              <NavLink
+                to="/admin"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <li>
+                  <FontAwesomeIcon icon={faUsers} />
+                  Users
+                </li>
+              </NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <li>
+                  <FontAwesomeIcon icon={faWallet} />
+                  Transactions
+                </li>
+              </NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <li>
+                  <FontAwesomeIcon icon={faPlus} />
+                  Add New Car
+                </li>
+              </NavLink>
+            </ul>
+
+            <p className="logoutButton">
+              <FontAwesomeIcon icon={faArrowRightFromBracket} />
+              Log out
+            </p>
+          </div>
+
+          <div className="adminPage">{children}</div>
+        </div>
+      </FullPageLayout>
+    </div>
   );
 };
 
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -3,7 +3,76 @@ import AdminLayout from "../../layout/AdminLayout/AdminLayout";
 const Admin = () => {
   return (
     <AdminLayout>
-      <p>Page</p>
+      <h1>
+        iciatis iure sint tempore? Deserunt, laudantium harum atque velit
+        veritatis temporibus ipsum fugit quaerat quidem, accusantium, recusandae
+        possimus omnis. Autem ex sit quasi unde quo itaque nesciunt. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Architecto dolorem nesciunt
+        dolor vitae temporibus similique voluptate, maxime perspiciatis quos
+        pariatur harum fuga consequuntur, atque asperiores cumque quibusdam
+        earum laborum optio. Officia, ipsum corrupti id dignissimos nisi fugiat
+        porro quae maxime quia, iure nostrum? Nulla maiores aliquam consequuntur
+        suscipit minus eum dolore unde inventore reprehenderit architecto
+        incidunt, possimus magni alias cumque. Excepturi quos ipsum nam animi
+        quasi nisi, beatae delectus doloribus facere explicabo, odio ex
+        praesentium architecto voluptates nihil, eaque nemo? Neque nihil
+        deleniti impedit dicta veniam laudantium dolorum, modi quos. Quas
+        molestiae ullam hic aspernatur pariatur explicabo cumque reiciendis
+        corporis saepe voluptate. Sit corrupti, minus tempora officiis ipsum eos
+        quae placeat aspernatur quasi voluptatibus itaque modi excepturi
+        consectetur rem nostrum. Vitae perferendis illo quo beatae, quisquam,
+        sequi incidunt autem repudiandae nesciunt a pariatur nemo vel officiis
+        fugiat iure cupiditate repellendus. Porro libero natus, iste eos magnam
+        iure error ipsam dolores? Provident soluta excepturi, eos distinctio
+        numquam facere debitis dolores iure officiis voluptatibus, sunt dolorem?
+        Iusto officiis recusandae cupiditate alias nostrum consequuntur quidem
+        nihil eius architecto, soluta ex assumenda numquam fugiat. Deserunt
+        eaque minima reprehenderit eos ab fugiat ratione aperiam aliquam quia,
+        id molestias modi saepe, voluptatum aspernatur harum laborum officiis
+        molestiae praesentium quam perspiciatis maxime corporis, quaerat atque
+        aut. Laborum? Odit magni eveniet laudantium iure harum quos, voluptas
+        excepturi perspiciatis voluptatum in temporibus soluta molestias!
+        Reiciendis facilis quos consectetur, quo, quam deleniti libero possimus
+        voluptatum ipsa nihil harum, quibusdam ipsum? Accusantium, tempore
+        delectus sunt est repellendus nihil necessitatibus? Voluptates sequi aut
+        repellendus veritatis impedit odit natus, esse ipsum, dolorum tempore
+        iste voluptatum dolores id unde quia molestiae reprehenderit
+        necessitatibus minus. Corporis a tempora quam odio magnam! Labore quae
+        nisi saepe numquam voluptate blanditiis ullam dolores fugit voluptas
+        nulla quod non, tempore natus aliquid temporibus, sed id ipsum, pariatur
+        commodi atque! Ex tenetur omnis nemo aperiam, reiciendis fugit, quis
+        cumque minus voluptatibus totam dolore sequi tempore autem reprehenderit
+        sapiente iure ab veritatis. Repudiandae voluptatem qui velit sequi esse
+        beatae debitis incidunt. Tempora, adipisci quod. Laborum nemo, voluptas
+        fugit praesentium, dignissimos veniam minima reiciendis soluta
+        consequatur iure sequi ut voluptatum, officia voluptatem dolorem magnam
+        ratione earum sit! Sapiente ut nam repudiandae dolorum! Deserunt
+        aliquid, praesentium quam ducimus eum debitis blanditiis autem ea
+        voluptatum quaerat, voluptatem illo quos accusamus natus unde libero
+        minus ut quasi quas! Repellendus animi adipisci voluptatem aliquid qui
+        molestiae? In eum aliquam voluptate, expedita, nihil tempora, odit natus
+        voluptatum architecto temporibus laborum recusandae. Rem laudantium
+        cupiditate alias omnis ullam voluptatum enim expedita sequi ipsam,
+        delectus quia laboriosam molestiae doloremque? Quia eos asperiores est
+        aliquid alias iusto non neque, nam amet natus quas atque autem
+        architecto ad unde repellendus praesentium vero rem nihil vel? Ullam sit
+        molestias nobis deserunt vel. Ipsum nisi id animi optio atque, quisquam,
+        quaerat ex velit cum corporis a earum nam voluptas amet consectetur
+        porro minima doloremque praesentium? Accusamus aut, laborum expedita
+        iusto unde quibusdam. Quas? Dicta nulla ipsam corrupti voluptas eius in
+        excepturi nisi esse qui laudantium, distinctio accusantium ea. Aperiam,
+        est praesentium. Deserunt natus suscipit recusandae quam enim fugit at
+        facilis tempore ipsum eligendi! Earum fuga atque labore eius iste ipsa
+        porro saepe. Quasi eveniet consequuntur, quibusdam magni quo, earum odit
+        sequi eos error nemo sit, veniam esse dolorem? Pariatur neque doloremque
+        animi adipisci. Facilis, minus omnis ea cum ducimus dolorem praesentium
+        fugiat architecto iure perferendis dolore, ut distinctio. Quod, ipsum
+        optio. Autem tempora, sint recusandae veniam repellat officiis
+        consequatur assumenda odit sed voluptas. Cum, enim sit. Dolorem iste
+        suscipit veritatis fugiat, debitis facilis sequi deleniti error sed
+        molestiae in rerum recusandae quas illo voluptatibus aut consectetur
+        repellat! Deleniti nulla sunt iusto saepe eius!
+      </h1>
     </AdminLayout>
   );
 };
