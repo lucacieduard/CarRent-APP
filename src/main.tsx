d83c0d8494f57b12085a164d57ef19c3f4e8f633
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-10-25 18:34:29 +0300

    context & cars table

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext.tsx";
+import { CarsContextProvider } from "./context/carsContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <CarsContextProvider>
+          <App />
+        </CarsContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/AdminCars/AdminCars.tsx
+++ src/pages/AdminCars/AdminCars.tsx
@@ -1,32 +1,68 @@
-import { ThemeProvider, createTheme } from "@mui/material";
 import AdminLayout from "../../layout/AdminLayout/AdminLayout";
 import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { CarsContext } from "../../context/carsContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Car } from "../../types/Car";
 
 const AdminCars = () => {
+  const navigate = useNavigate();
+  const carsContext = useContext(CarsContext);
+  console.log(carsContext);
+
   return (
     <AdminLayout>
-      <div style={{ maxWidth: "100%" }}>
+      <div style={{ width: "99%", maxHeight: "500px" }}>
         <ThemeProvider theme={createTheme()}>
           <MaterialTable
             columns={[
-              { title: "Adı", field: "name" },
-              { title: "Soyadı", field: "surname" },
-              { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
               {
-                title: "Doğum Yeri",
-                field: "birthCity",
-                lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+                title: "Image",
+                field: "svg",
+                render: (rowData) => <img src={rowData.svg} width={85} />,
+              },
+              { title: "Name", field: "carName" },
+              {
+                title: "Price",
+                field: "price",
+                render: (rowData) => <p>${rowData.price}</p>,
+              },
+              { title: "Car Type", field: "carType" },
+            ]}
+            data={carsContext.cars as Car[]}
+            title="Cars"
+            isLoading={carsContext.loading}
+            actions={[
+              {
+                icon: "refresh",
+                tooltip: "Refresh Cars",
+                onClick: () => carsContext.refresh(),
+                position: "toolbar",
+              },
+              {
+                icon: "edit",
+                tooltip: "Edit Car",
+                onClick: (_event, rowData) => {
+                  if (!Array.isArray(rowData) && rowData.uid) {
+                    navigate(`/admin/add?id=${rowData.uid}`);
+                  }
+                },
+                position: "row",
+              },
+              {
+                icon: "delete",
+                tooltip: "Delete Car",
+                onClick: () => carsContext.refresh(),
+                position: "row",
               },
             ]}
-            data={[
-              {
-                name: "Mehmet",
-                surname: "Baran",
-                birthYear: 1987,
-                birthCity: 63,
-              },
-            ]}
-            title="Demo Title"
+            options={{
+              actionsColumnIndex: -1,
+              doubleHorizontalScroll: true,
+              maxBodyHeight: 500,
+              emptyRowsWhenPaging: false,
+            }}
           />
         </ThemeProvider>
       </div>
