ae44a732e17da8316a719ababba47689ec6cd73c
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-10-25 18:28:01 +0300

    deploy test

--- src/pages/AdminCars/AdminCars.tsx
+++ src/pages/AdminCars/AdminCars.tsx
@@ -1,7 +1,37 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import AdminLayout from "../../layout/AdminLayout/AdminLayout";
+import MaterialTable from "material-table";
 
 const AdminCars = () => {
-  return <AdminLayout>AdminCars</AdminLayout>;
+  return (
+    <AdminLayout>
+      <div style={{ maxWidth: "100%" }}>
+        <ThemeProvider theme={createTheme()}>
+          <MaterialTable
+            columns={[
+              { title: "Adı", field: "name" },
+              { title: "Soyadı", field: "surname" },
+              { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+              {
+                title: "Doğum Yeri",
+                field: "birthCity",
+                lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+              },
+            ]}
+            data={[
+              {
+                name: "Mehmet",
+                surname: "Baran",
+                birthYear: 1987,
+                birthCity: 63,
+              },
+            ]}
+            title="Demo Title"
+          />
+        </ThemeProvider>
+      </div>
+    </AdminLayout>
+  );
 };
 
 export default AdminCars;
