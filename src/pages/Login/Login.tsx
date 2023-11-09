46b764894237ce0aab8e40d35520e8259dd84767
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-11-09 11:59:04 +0200

    CMS login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,18 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import AuthLayout from "../../layout/Auth/AuthLayout";
 import "./Login.scss";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { AuthContext } from "../../context/authContext";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const path = useLocation();
+  const user = useContext(AuthContext);
+  console.log(path);
 
   const navigate = useNavigate();
 
@@ -17,7 +21,11 @@ const Login = () => {
     setLoading(true);
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
+      if (path.search.includes("redirect=true")) {
+        navigate("/admin");
+      } else {
+        navigate("/");
+      }
     } catch (error) {
       setError(true);
     } finally {
@@ -25,12 +33,16 @@ const Login = () => {
     }
   };
 
+  useEffect(() => {
+    if (path.search.includes("redirect=true")) {
+      setEmail("test@test.com");
+      setPassword("testtest");
+    }
+  }, []);
+
   return (
     <AuthLayout>
-      <div
-        className="formContainer"
-        
-      >
+      <div className="formContainer">
         <div className="text">
           <p>Welcome Back</p>
 
@@ -41,17 +53,27 @@ const Login = () => {
             type="email"
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
           <input
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
-          <button type="submit" disabled={loading}>
+          <button
+            type="submit"
+            disabled={loading || user.user?.email ? true : false}
+          >
             Login!
           </button>
           {error && (
             <span style={{ color: "red" }}>Wrong email or password!</span>
+          )}
+          {user.user?.email && (
+            <span style={{ color: "green" }}>
+              You are already logged in as {user.user?.email}
+            </span>
           )}
         </form>
         <span className="nAc">
