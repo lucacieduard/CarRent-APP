2c5c1cf2e3d2faabc347e2ac03286f9d2b50b3b6
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-11-30 21:26:08 +0200

    filter functionality

--- src/layout/SideBarPage/SideBarPage.tsx
+++ src/layout/SideBarPage/SideBarPage.tsx
@@ -4,11 +4,52 @@ import { useState } from "react";
 const SideBarPage = ({
   buttons,
   children,
+  filter,
 }: {
   buttons: boolean;
   children: React.ReactNode;
+  filter: (
+    type: string[],
+    capacity: number[],
+    price: number | undefined
+  ) => void;
 }) => {
   const [showSideBar, setShowSideBar] = useState<boolean>(false);
+  const [filters, setFilters] = useState<{
+    type: string[];
+    capacity: number[];
+    price: number | undefined;
+  }>({
+    type: [],
+    capacity: [],
+    price: undefined,
+  });
+
+  const changeFilter = (
+    checked: boolean,
+    value: string | number,
+    param: "type" | "capacity"
+  ) => {
+    if (checked) {
+      (filters[param] as (string | number)[]).includes(value)
+        ? ""
+        : setFilters((prev) => {
+            return {
+              ...prev,
+              [param]: [...prev[param], value],
+            };
+          });
+    } else {
+      (filters[param] as (string | number)[]).includes(value)
+        ? setFilters((prev) => {
+            return {
+              ...prev,
+              [param]: [...prev.type.filter((type) => type !== value)],
+            };
+          })
+        : "";
+    }
+  };
 
   return (
     <FullPageLayout>
@@ -28,69 +69,154 @@ const SideBarPage = ({
             <div className="section">
               <span className="sectionTitle">Type</span>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value="sport"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                />
                 <span className="sectionItemName">Sport</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value="suv"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                />
                 <span className="sectionItemName">SUV</span>
-                <span className="sectionItemLen">(12)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value="mpv"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                />
                 <span className="sectionItemName">MPV</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value="sedan"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                />
                 <span className="sectionItemName">Sedan</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value="coupe"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                />
                 <span className="sectionItemName">Coupe</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  onChange={(e) =>
+                    changeFilter(e.target.checked, e.target.value, "type")
+                  }
+                  value="hatchback"
+                />
                 <span className="sectionItemName">Hatchback</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
             </div>
             <div className="section">
               <span className="sectionTitle">Capacity</span>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value={2}
+                  onChange={(e) =>
+                    changeFilter(
+                      e.target.checked,
+                      Number(e.target.value),
+                      "capacity"
+                    )
+                  }
+                />
                 <span className="sectionItemName">2 Person</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value={4}
+                  onChange={(e) =>
+                    changeFilter(
+                      e.target.checked,
+                      Number(e.target.value),
+                      "capacity"
+                    )
+                  }
+                />
                 <span className="sectionItemName">4 Person</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value={6}
+                  onChange={(e) =>
+                    changeFilter(
+                      e.target.checked,
+                      Number(e.target.value),
+                      "capacity"
+                    )
+                  }
+                />
                 <span className="sectionItemName">6 Person</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
               <div className="sectionItem">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  value={8}
+                  onChange={(e) =>
+                    changeFilter(
+                      e.target.checked,
+                      Number(e.target.value),
+                      "capacity"
+                    )
+                  }
+                />
                 <span className="sectionItemName">8 or More</span>
-                <span className="sectionItemLen">(10)</span>
               </div>
             </div>
             <div className="section">
               <span className="sectionTitle">Price</span>
               <div className="range">
-                <input type="range" min={0} max={100} />
-                <span className="max">Max : 100</span>
+                <input
+                  type="range"
+                  min={0}
+                  max={150}
+                  onChange={(e) => {
+                    setFilters((prev) => {
+                      return {
+                        ...prev,
+                        price: Number(e.target.value),
+                      };
+                    });
+                  }}
+                />
+                <span className="max">Max : 150</span>
               </div>
             </div>
           </div>
           {buttons && (
-            <button className="button w" onClick={() => setShowSideBar(false)}>
+            <button
+              className="button w"
+              onClick={() => {
+                setShowSideBar(false);
+
+                filter(filters.type, filters.capacity, filters.price);
+              }}
+            >
               Apply Filters
             </button>
           )}
--- src/pages/Cars/Cars.tsx
+++ src/pages/Cars/Cars.tsx
@@ -10,10 +10,13 @@ import Loading from "../../components/Loading/Loading";
 import { AnimatePresence } from "framer-motion";
 import { containerVariants } from "../../utils/containerVariants";
 import { motion } from "framer-motion";
+import { Car } from "../../types/Car";
 
 const Cars = () => {
   const [carsNumber, setCarsNumber] = useState(8);
   const carsContext = useContext(CarsContext);
+  const [filtered, setFiltered] = useState<Car[]>([]);
+  const [fil, setFil] = useState<boolean>(false);
   const clickHandler = (add: boolean) => {
     if (add && carsNumber > carsContext.cars.length) return;
     if (add && carsNumber + 8 > carsContext.cars.length) {
@@ -25,6 +28,35 @@ const Cars = () => {
     }
   };
 
+  const setFilteredCars = (
+    type: string[],
+    capacity: number[],
+    price: number | undefined
+  ) => {
+    let cars: Car[] = [];
+
+    if (price) {
+      cars = carsContext.cars.filter((car) => car.price <= price);
+
+      if (type.length > 0) {
+        cars = cars.filter((car) => type.includes(car.carType));
+      }
+      if (capacity.length > 0) {
+        cars = cars.filter((car) => capacity.includes(car.capacity));
+      }
+    } else {
+      cars = carsContext.cars;
+      if (type.length > 0) {
+        cars = cars.filter((car) => type.includes(car.carType));
+      }
+      if (capacity.length > 0) {
+        cars = cars.filter((car) => capacity.includes(car.capacity));
+      }
+    }
+    setFil(true);
+    setFiltered(cars);
+  };
+
   return (
     <motion.div
       className="bg"
@@ -33,12 +65,27 @@ const Cars = () => {
       animate="visible"
       exit="exit"
     >
-      <SideBarPage buttons={true}>
+      <SideBarPage buttons={true} filter={setFilteredCars}>
         <FilterSection />
         <div className="carsContainer">
           <AnimatePresence>
             {carsContext.cars.length === 0 ? (
               <Loading />
+            ) : fil ? (
+              filtered.length > 0 ? (
+                filtered.map((car, index) => {
+                  return (
+                    <CarCard
+                      key={index}
+                      nr={index}
+                      recomandation={false}
+                      car={car}
+                    />
+                  );
+                })
+              ) : (
+                <p>No car found</p>
+              )
             ) : (
               carsContext.cars.slice(0, carsNumber).map((car, index) => {
                 return (
@@ -55,7 +102,7 @@ const Cars = () => {
         </div>
 
         <div className="buttons">
-          {carsNumber < carsContext.cars.length && (
+          {carsNumber < carsContext.cars.length && !fil && (
             <button
               style={{ cursor: "pointer" }}
               onClick={() => clickHandler(true)}
@@ -63,7 +110,7 @@ const Cars = () => {
               Show more cars
             </button>
           )}
-          {carsNumber > 8 && (
+          {carsNumber > 8 && !fil && (
             <button
               style={{ cursor: "pointer" }}
               onClick={() => clickHandler(false)}
